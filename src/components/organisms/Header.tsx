5fe3e2ea01ef04f6e59121a07ac0e58429f1243a
Theo van Oostrum <gt.oostrum@gmail.com>
2025-12-18 22:41:29 +0100

    bigger logo

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -10,10 +10,10 @@ export function Header(): React.ReactElement {
   const t = useTranslations("header");
 
   return (
-    <header className="bg-[#ffc9e1] px-1 py-2 flex justify-between items-center">
+    <header className="bg-[#ffc9e1] px-1 py-2 md:px-4 md:py-3 flex justify-between items-center">
       <LanguageSwitcher />
-      <div className="mr-4">
-        <div className="relative w-[41px] h-[49px]">
+      <div className="mr-4 md:mr-6">
+        <div className="relative w-[50px] h-[60px] md:w-[80px] md:h-[96px]">
           <Image
             src="/images/logo.png"
             alt="Irongirl Logo"
